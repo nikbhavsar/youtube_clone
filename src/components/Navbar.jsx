a3b48f50d2085549a09a9a910db28053e3c6ac9b
Nikhar Bhavsar <bhavsarnikhar21@gmail.com>
2023-08-26 23:47:45 -0700

    Project Setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from '@mui/material'
+
+import { logo } from '../utils/constants'
+import SearchBar from './SearchBar'
+
+
+const Navbar = () => (
+	<Stack
+		direction="row"
+		alignItems="center" p={2}
+		sx={{ position: 'sticky', background: "#000", to: 0, justifyContent: 'space-between' }}>
+
+		<Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+			<img src={logo} alt='logo' height={45}></img>
+		</Link>
+	<SearchBar />
+	</Stack>
+)
+
+export default Navbar
